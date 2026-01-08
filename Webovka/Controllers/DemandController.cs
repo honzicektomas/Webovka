ec8760915403b67aad15b1530a40944399175946
honzicektomas <honzicektomas@sssvt.cz>
2026-01-08 19:25:05 +0100

    Add project files.

---
+++ Webovka/Controllers/DemandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webovka.Controllers
+{
+    public class DemandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
