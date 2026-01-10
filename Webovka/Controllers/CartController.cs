8afac7a4f58b84b37609290ad669b08b0de87be8
honzicektomas <honzicektomas@sssvt.cz>
2026-01-10 23:56:50 +0100

    index fix

---
+++ Webovka/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Webovka.Models;
+using System.Linq;
+
+namespace Webovka.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Zde později načteme data z DB
+            using (var db = new MyContext())
+            {
+                // Zatím pošleme prázdný seznam, aby stránka nepadala
+                return View(new List<OrderItem>());
+            }
+        }
+    }
+}
